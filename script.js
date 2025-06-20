82a32554dc1da0d3f373840b55daa969e5f2b2ee
Duc Huy <NHuyyi@users.noreply.github.com>
2025-06-20 18:07:16 +0700

    lan_cuoi

--- script.js
+++ script.js
@@ -1,62 +1,3 @@
-// const video1 = document.getElementById('projectVideo1');
-// const video2 = document.getElementById('projectVideo2');
-// const video3 = document.getElementById('projectVideo3');
-
-// Sidebar elements //
-// const sideBar = document.querySelector('.sidebar');
-// const menu = document.querySelector('.menu-icon');
-// const closeIcon = document.querySelector('.close-icon')
-
-
-// const hoverSign = document.querySelector('.hover-sign');
-
-// const videoList =[video1, video2, video3];
-
-// videoList.forEach (function(video){
-//     video.addEventListener("mouseover", function(){
-//         video.play()
-//         hoverSign.classList.add("active")
-//     })
-//     video.addEventListener("mouseout", function(){
-//     video.pause();
-//     hoverSign.classList.remove("active")
-// })
-// })
-
-// Sidebar elements //
-// menu.addEventListener("click", function(){
-//     sideBar.classList.remove("close-sidebar")
-//     sideBar.classList.add("open-sidebar")
-// });
-
-// closeIcon.addEventListener("click", function(){
-//     sideBar.classList.remove("open-sidebar");
-//     sideBar.classList.add("close-sidebar");
-    
-// })
-
-
-
-
-// Video hover play/pause
-const video1 = document.getElementById('projectVideo1');
-const video2 = document.getElementById('projectVideo2');
-const video3 = document.getElementById('projectVideo3');
-const hoverSign = document.querySelector('.hover-sign');
-const videoList = [video1, video2, video3];
-
-videoList.forEach(video => {
-  video.addEventListener('mouseover', () => {
-    video.play();
-    hoverSign.classList.add('active');
-  });
-  video.addEventListener('mouseout', () => {
-    video.pause();
-    hoverSign.classList.remove('active');
-  });
-});
-
-// Sidebar toggle
 const sideBar = document.querySelector('.sidebar');
 const menu = document.querySelector('.menu-icon');
 const closeIcon = document.querySelector('.close-icon');
@@ -70,10 +11,8 @@ closeIcon.addEventListener('click', () => {
   sideBar.classList.add('close-sidebar');
 });
 
-// Initialize EmailJS (after including its SDK in HTML)
 emailjs.init("YOUR_USER_ID");
 
-// Contact form submission
 const contactForm = document.getElementById('contact-form');
 if (contactForm) {
   contactForm.addEventListener('submit', e => {
